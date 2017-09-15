928cdef2c8c22ba0ab66fbc2d1d685f3712b7cee
Michael Ron Lao <michaelron.lao@benilde.edu.ph>
2017-09-15 13:59:48 +0800

    09/15/17

---
+++ MyBookstore/Models/AuthorsModels.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyBookstore.Models
+{
+    public class AuthorsModels
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name ="Last Name")]
+        [MaxLength(40, ErrorMessage = "Up to 40 characters only!")]
+        [Required(ErrorMessage = "Required!")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Required!")]
+        [MaxLength(20, ErrorMessage = "Up to 20 characters only!")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Phone")]
+        [Required(ErrorMessage = "Required!")]
+        [MaxLength(12, ErrorMessage = "Up to 12 characters only!")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Address")]
+        [MaxLength(40, ErrorMessage = "Up to 40 characters only!")]     
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+
+        [Display(Name = "City")]
+        [MaxLength(20, ErrorMessage = "Up to 20 characters only!")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        [MaxLength(2, ErrorMessage = "Up to 2 characters only!")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        [MaxLength(5, ErrorMessage = "Up to 5 characters only!")]
+        public string Zip { get; set; }
+    }
+}
