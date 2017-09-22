a72b0f7db779e73599c34dc120af0e4d053a29a9
Michael Ron Lao <michaelron.lao@benilde.edu.ph>
2017-09-22 14:07:36 +0800

    9/22/2017

--- MyBookstore/App_Start/BundleConfig.cs
+++ MyBookstore/App_Start/BundleConfig.cs
@@ -22,10 +22,10 @@ namespace MyBookstore
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", "~/Content/font-awesome.min.css"));
         }
     }
 }
